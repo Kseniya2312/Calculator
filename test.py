0bb6c866fcdcf010454b773a3b2aca6046e2363c
Kseniya <kseniya2312@gmail.com>
2025-03-03 22:36:00 +0300

    файл с тестами

--- test.py
+++ test.py
@@ -11,13 +11,13 @@ class TestCalc:
         assert self.calc.adding(self, 1, 1) == 2
 
     def test_division_success(self):
-        assert self.calc.division(self,8,4) == 2
+        assert self.calc.division(self, 8, 4) == 2
 
     def test_multiplication_success(self):
-        assert self.calc.multiply(self,6,2) == 12
+        assert self.calc.multiply(self, 6, 2) == 12
 
     def test_subtract_success(self):
-        assert  self.calc.subtraction(self,10,5)== 5
+        assert  self.calc.subtraction(self, 10, 5)== 5
 
     def test_zero_division(self):
         with pytest.raises(ZeroDivisionError):
